7ada74c96fbbe7bda8a28ecf7c1b5879faed5c43
enbraining <me@xolving.com>
2024-06-02 14:05:06 +0900

    header gap

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Header(){
     return (
-        <header className="flex gap-x-10 pb-24 font-semibold text-xl text-stone-500">
+        <header className="flex gap-x-[6.5rem] pb-24 font-semibold text-xl text-stone-500">
             <Link href={'/'}>소개</Link>
             <Link href={'/'}>블로그</Link>
             <Link href={'/'}>프로젝트</Link>
